0d5ea73eb1912d7f333265d540290337be90e0d5
Harish Nigam <harishnigam21@gmail.com>
2025-07-30 23:38:07 +0530

    basic setup completed

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corsOptions = require("./config/cors");
+const cookieParser = require("cookie-parser");
+const credentials = require("./middleware/credentials");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+
+//middleware
+app.use(credentials);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+//app
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Amrutam Backend Server");
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Your Amrutam backend is running on PORT no : ${PORT}`
+  );
+});
